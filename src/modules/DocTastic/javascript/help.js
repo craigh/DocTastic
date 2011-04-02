6ccb0f5bb8cd8969519c637e20dd6c9f837f441b
Craig Heydenburg <craigh@mac.com>
2011-04-01 22:29:08 -0400

    minor changes to js show/hide

--- src/modules/DocTastic/javascript/help.js
+++ src/modules/DocTastic/javascript/help.js
@@ -11,9 +11,7 @@ function doctastic_help_load()
 function doctastic_help_init()
 {
     $('doctastic_help_collapse').observe('click', doctastic_help_click);
-//    $('doctastic_help_collapse').addClassName('z-toggle-link');
     if ($('doctastic_help_container').style.display != "none") {
-//        $('doctastic_help_collapse').removeClassName('z-toggle-link-open');
         $('doctastic_help_showhide').update(Zikula.__('Help','module_doctastic'));
         $('doctastic_help_container').hide();
     }
@@ -22,11 +20,9 @@ function doctastic_help_init()
 function doctastic_help_click()
 {
     if ($('doctastic_help_container').style.display != "none") {
-//        Element.removeClassName.delay(0.9, $('doctastic_help_collapse'), 'z-toggle-link-open');
-        $('doctastic_help_showhide').update(Zikula.__('Help','module_doctastic'));
+        Element.update.delay(0.9, 'doctastic_help_showhide', Zikula.__('Help','module_doctastic'));
     } else {
-//        $('doctastic_help_collapse').addClassName('z-toggle-link-open');
-        $('doctastic_help_showhide').update(Zikula.__('Hide Help','module_doctastic'));
+        Element.update.delay(0.9, 'doctastic_help_showhide', Zikula.__('Hide Help','module_doctastic'));
     }
     switchdisplaystate('doctastic_help_container');
 }
