21064a7314975e6dc98edd69d09e754433063017
Craig Heydenburg <craigh@mac.com>
2011-04-01 21:34:11 -0400

    add style to help link.

--- src/modules/DocTastic/javascript/help.js
+++ src/modules/DocTastic/javascript/help.js
@@ -11,10 +11,10 @@ function doctastic_help_load()
 function doctastic_help_init()
 {
     $('doctastic_help_collapse').observe('click', doctastic_help_click);
-    $('doctastic_help_collapse').addClassName('z-toggle-link');
+//    $('doctastic_help_collapse').addClassName('z-toggle-link');
     if ($('doctastic_help_container').style.display != "none") {
-        $('doctastic_help_collapse').removeClassName('z-toggle-link-open');
-        $('doctastic_help_showhide').update(Zikula.__('Show','module_doctastic'));
+//        $('doctastic_help_collapse').removeClassName('z-toggle-link-open');
+        $('doctastic_help_showhide').update(Zikula.__('Help','module_doctastic'));
         $('doctastic_help_container').hide();
     }
 }
@@ -22,11 +22,11 @@ function doctastic_help_init()
 function doctastic_help_click()
 {
     if ($('doctastic_help_container').style.display != "none") {
-        Element.removeClassName.delay(0.9, $('doctastic_help_collapse'), 'z-toggle-link-open');
-        $('doctastic_help_showhide').update(Zikula.__('Show','module_doctastic'));
+//        Element.removeClassName.delay(0.9, $('doctastic_help_collapse'), 'z-toggle-link-open');
+        $('doctastic_help_showhide').update(Zikula.__('Help','module_doctastic'));
     } else {
-        $('doctastic_help_collapse').addClassName('z-toggle-link-open');
-        $('doctastic_help_showhide').update(Zikula.__('Hide','module_doctastic'));
+//        $('doctastic_help_collapse').addClassName('z-toggle-link-open');
+        $('doctastic_help_showhide').update(Zikula.__('Hide Help','module_doctastic'));
     }
     switchdisplaystate('doctastic_help_container');
 }
--- src/modules/DocTastic/templates/admin/help.tpl
+++ src/modules/DocTastic/templates/admin/help.tpl
@@ -1,6 +1,11 @@
 {ajaxheader modname='DocTastic' filename='help.js'}
-<div id='doctastic_help_container' class='doctastic_container' style='height:100px;background-color:red;'>
+{pageaddvar name='stylesheet' value='modules/DocTastic/style/helpstyle.css'}
+<div id='doctastic_help_container' style='height:100px;background-color:red;'>
     <h1>{gt text="DocTastic theme.prefooter"}</h1>
     <h2>{$topmodule}</h2>
 </div>
-<a id="doctastic_help_collapse" href="javascript:void(0);"><span id="doctastic_help_showhide">{gt text='Help'}</span></a>
+<div id='doctastic_linkrow'><span class='sub'>
+    <a id="doctastic_help_collapse" class='doctastic_helplink' href="javascript:void(0);">
+	<span class='z-icon-es-help' id="doctastic_help_showhide">{gt text='Help'}</span>
+    </a>
+</span></div>
