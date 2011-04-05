3c72e953226c3518bf28cef71ded42488243ce81
Craig Heydenburg <craigh@mac.com>
2011-04-05 17:46:35 -0400

    minor code formatting

--- src/modules/DocTastic/lib/DocTastic/Handlers.php
+++ src/modules/DocTastic/lib/DocTastic/Handlers.php
@@ -43,7 +43,7 @@ class DocTastic_Handlers
      */
     public static function renderHelp(Zikula_Event $event)
     {
-	$enableInlineHelp = ModUtil::getVar('DocTastic', 'enableInlineHelp', false);
+        $enableInlineHelp = ModUtil::getVar('DocTastic', 'enableInlineHelp', false);
         $type = FormUtil::getPassedValue('type', 'user', 'GETPOST');
         if (($enableInlineHelp) && ($type == 'admin')) {
             $view = $event->getSubject();
