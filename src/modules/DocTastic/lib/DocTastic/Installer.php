2a5efe5276f908a241beefa12d4dbe4ccf8edcef
Craig Heydenburg <craigh@mac.com>
2011-04-10 07:47:10 -0400

    change event name to match core.

--- src/modules/DocTastic/lib/DocTastic/Installer.php
+++ src/modules/DocTastic/lib/DocTastic/Installer.php
@@ -35,7 +35,7 @@ class DocTastic_Installer extends Zikula_AbstractInstaller
 
         EventUtil::registerPersistentModuleHandler('DocTastic', 'module_dispatch.service_links', array('DocTastic_Handlers', 'servicelinks'));
         EventUtil::registerPersistentModuleHandler('DocTastic', 'module.doctastic.gettypes', array('DocTastic_Handlers', 'getTypes'));
-        EventUtil::registerPersistentModuleHandler('DocTastic', 'theme.prefooter', array('DocTastic_Handlers', 'renderHelp'));
+        EventUtil::registerPersistentModuleHandler('DocTastic', 'theme.prefetch', array('DocTastic_Handlers', 'renderHelp'));
 
         return true;
     }
